3a1c843194a04b8bc51b12e11a9aeecc3a0e77be
SajithMohan2772 <sajith2772@gmail.com>
2023-10-01 14:05:20 +0530

    2nd commit

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,10 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+    const reversedStr = formattedStr.split('').reverse().join('');
+
+    return formattedStr === reversedStr;
+}
+
 
 module.exports = isPalindrome;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,21 @@
-function removeDuplicates() {}
+// function removeDuplicates(arr) {
+    
+//     const uniqueArr = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//     if (!uniqueArr.includes(arr[i])) {
+//         uniqueArr.push(arr[i]);
+//     }
+//     }
+
+
+//     return uniqueArr;
+// }
+function removeDuplicates(arr){
+    return Array.from(new Set(arr));
+}
+
+
 
 module.exports = removeDuplicates;
