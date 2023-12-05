972bf38cbce2c8bfc030dc7aff65bc64eba52f56
SajithMohan2772 <sajith2772@gmail.com>
2023-12-05 21:25:44 +0530

    bc-2 commit

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,15 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+    const charCount = {};
+    
+    for (let i = 0; i < str.lenght; i++){
+        const char = str[i];
+
+        if (charCount[char]) {
+            return false;
+        }
+        charCount[char] = true;
+    }
+    return true;
+}
 
 module.exports = areAllCharactersUnique;
